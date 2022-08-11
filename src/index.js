d95849fa53b194442618402f1827bfb9de2e3c57
Rikstone <tarikbouari@gmail.com>
2022-08-11 11:37:09 +0000

    update dist files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import './style.css';
+import "./style.css";
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
 
   // Lodash, now imported by this script
-  element.innerHTML = '<h1>heloo</h1>';
+  element.innerHTML = "<h1>heloo</h1>";
 
   return element;
 }
-
-document.body.appendChild(component());
