c5a6b4721df25e69b6c56e567b0f25cd8212718b
Rikstone <tarikbouari@gmail.com>
2022-08-11 17:44:51 +0000

    add with to placeholder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import "./style.css";
+import List from "./class.js";
+export const arr = [
+  {
+    description: " Submit list project one1",
+    completed: true,
+    index: "1",
+  },
+  {
+    description: " Setup webpack file ",
+    completed: true,
+    index: "2",
+  },
+  {
+    description: "Get project approved ",
+    completed: false,
+    index: "3",
+  },
+];
 
-function component() {
-  const element = document.createElement("div");
+const add = document.getElementById("send");
+const container = document.getElementById("task");
+const inputAdd = document.getElementById("add");
+const form = document.getElementById("form");
 
-  // Lodash, now imported by this script
-  element.innerHTML = "<h1>heloo</h1>";
+const loader = () => {
+  const load = arr
+    .map((item, i) => {
+      return ` <div class="flex">
+                  <div class="py-3"> 
+                  <input type="checkbox" id="description">
+                  <label for="description" class="mx-3"> ${item.description}</label>
+                  </div>
+                  <span class="material-symbols-outlined">
+                  more_vert
+                  </span>
+                </div>`;
+    })
+    .join("");
 
-  return element;
-}
+  container.innerHTML = load;
+};
+
+loader();
+
+add.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(true);
+  let desValue = inputAdd.value;
+  if (!desValue) return "value missing";
+
+  let newList = new List(desValue);
+  newList.addToList();
+  loader();
+  form.reset();
+});
