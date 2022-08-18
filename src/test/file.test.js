6ee3fbdf9b5839f782311dc59b13cde2f5078b1e
Rikstone <tarikbouari@gmail.com>
2022-08-18 16:14:27 +0000

    create test file

---
+++ src/test/file.test.js
@@ -0,0 +1,13 @@
+const addList = require("./addRemove.js");
+
+let array = [];
+describe("add and remove", () => {
+  // testing add
+  test(" add item to list", () => {
+    array = addList.add(array, "test");
+    expect(array).toHaveLength(1);
+
+    array = addList.add(array, "test2");
+    expect(array).toHaveLength(2);
+  });
+});
